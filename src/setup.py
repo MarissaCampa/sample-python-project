6dbd1396ff44849f300b53d57c0f649bafa10a29
gisel <gisel@Ubuntu>
2023-06-16 03:22:54 -0400

    Added a script to create a binary

---
+++ src/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='simple_api',
+      version='1.0',
+      description='A Simple API',
+      author='Marissa Campa',
+      author_email='marissag.campa@gmail.com',
+      url='https://www.linkedin.com/in/marissa-campa/',
+      packages=find_packages(),
+    )
